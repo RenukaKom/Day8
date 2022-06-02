0d3163533bde1d2f3845bad51db0bc04ed7c98e7
chaudharineha2108@gmail.com <chaudharineha2108@gmail.com>
2022-06-02 23:27:16 +0530

    Attached Day8 Assignment

--- dicerandom.sh
+++ dicerandom.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
-w=0;
-count=0;
-declare -a dict
-while [ $w -lt 10 ]
+dictionary[1]=0
+dictionary[2]=0
+dictionary[3]=0
+dictionary[4]=0
+dictionary[5]=0
+dictionary[6]=0
+max_count=10
+while(( 10 ))
+do
+    res=$(( RANDOM % 6 + 1 ))
+    (( dictionary[$res]++))
+
+    if(( dictionary[$res] == max_count))
+    then
+        break
+    fi
+done 
+most_frequent_number=$(( 1+RANDOM % 6 ))
+least_frequent_number=$(( 1+RANDOM % 6 ))
+
+echo " corresponding frequencies are:"
+echo ${!dictionary[@]}
+for number in ${!dictionary[@]}
 do
 
-	num=$(( RANDOM %6 + 1 ))
-        	echo " Random dice number is: " $num
-		echo ${dict[@]}
-		dict[(( count ++ ))]=$num
-	w=`expr $w + 1`
+    if(( dictionary[$number] >= dictionary[$most_frequent_number] ))
+    then
+        most_frequent_number=$number
+    fi
 
+    if(( dictionary[$number] <= dictionary[$least_frequent_number] ))
+    then
+        least_frequent_number=$number
+    fi
+
+    echo $number " " ${dictionary[$number]}
 done
-#echo "the dictionary inclues:"
-echo ${dict[@]}
 
+echo " maximum times is $most_frequent_number"
+echo "minimum times is $least_frequent_number"
